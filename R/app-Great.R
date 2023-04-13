edff57a7478bd80707c93de898c1fb1cf6e60cf2
Jonas Bucher <jobucher@student.ethz.ch>
2023-04-13 15:43:04 +0200

    param input to great function instead of default

--- R/app-Great.R
+++ R/app-Great.R
@@ -8,6 +8,12 @@
 
 ezMethodGreat <- function(input = NA, output = NA, param = NA,
                            htmlFile = "00index.html") {
+  
+  ### libraries
+  library("rGREAT")
+  library("KEGGREST")
+  library("biomaRt")
+  
   # #setwdNew(basename(output$getColumn("Report")))
   dataset <- input$meta
   ans4Report <- list() # a list of results for rmarkdown report
@@ -16,30 +22,78 @@ ezMethodGreat <- function(input = NA, output = NA, param = NA,
   # output_dir <- basename(output$getColumn("Report"))
   dataDir <- "~/data/dmrseq"
   dmRegionsFilePath <- file.path(dataDir, "dmRegions.rds")
-  # significantRegionsFilePath <- file.path(dataDir, "significantRegions.rds")
+  significantRegionsFilePath <- file.path(dataDir, "significantRegions.rds")
+  
   dmRegions <- readRDS(dmRegionsFilePath)
-  geneSets	<- "GO:BP"
-  tssSource <- ""
+  significantRegions <- readRDS(significantRegionsFilePath)
   
-  # mmusculus_gene_ensembl
+  ## Reactome pathways
+  if(param$reactome_kegg & param$biomart_dataset=="athaliana_eg_gene") {
+    reactome <- "https://plantreactome.gramene.org/download/current/gene_ids_by_pathway_and_species.tab"
+    react <- data.frame(data.table::fread(input = reactome, header = F, nThread = 16))
+    rdb <- react[grep(pattern = "^R-ATH", x = react$V1), ]
+    reactome_pathways <- split(rdb$V4, paste(rdb$V1, rdb$V2, sep = ": "))
+    
+    ## KEGG pathways
+    kg <- keggList("organism")
+    
+    pathway2gene <- keggLink("pathway", "ath")
+    pathwayName <- keggList("pathway", "ath")
+    df1 <- data.frame(
+      gene = gsub("ath:", "", names(pathway2gene)),
+      pathID = gsub("path:", "", pathway2gene)
+    )
+    
+    df2 <- data.frame(
+      pathID = gsub("path:", "", names(pathwayName)),
+      name = pathwayName
+    )
+    
+    df_kegg <- merge(df2, df1)
+    
+    kegg_pathways <- split(df_kegg$gene, paste(df_kegg$pathID, df_kegg$name,
+                                               sep = ": "
+    ))
+    
+    geneSetsAthaliana <- c("reactome_pathways" = reactome_pathways, "kegg_pathways" = kegg_pathways)
+    
+    # gs = read_gmt(url("http://dsigdb.tanlab.org/Downloads/D2_LINCS.gmt"), 
+    #               from = "SYMBOL", to = "ENTREZ", orgdb = "org.Hs.eg.db")
+    
+    ### TODO: add built-in gene set(s) to these
+    greatResult <- great(gr = dmRegions, gene_sets = geneSetsAthaliana, tss_source = "TxDb.Athaliana.BioMart.plantsmart51",
+                         min_gene_set_size = param$min_gene_set_size, mode = param$mode, basal_upstream = param$basal_upstream,
+                         basal_downstream = param$basal_downstream, extension = param$extension,
+                         background = dmRegions, exclude = param$exclude,
+                         cores = param$cores)
+  }
   
-  greatResult <- great(dmRegions, "GO:BP", biomart_dataset = "mmusculus_gene_ensembl")
+  else {
+    # tssSource <- ""
+    geneSets <- param$gene_sets
+    
+    greatResult <- great(gr = significantRegions, gene_sets = geneSets, biomart_dataset = biomart_dataset,
+                         min_gene_set_size = param$min_gene_set_size, mode = param$mode, basal_upstream = param$basal_upstream,
+                         basal_downstream = param$basal_downstream, extension = param$extension,
+                         background = dmRegions, exclude = param$exclude, # gap regions for corresponding organism will be removed from the analysis
+                         cores = param$cores)
+  }
   
-  greatResult <- great(gr = dmRegions, gene_sets = geneSets, tss_source = tssSource, biomart_dataset = NULL,
-        min_gene_set_size = 5, mode = "basalPlusExt", basal_upstream = 5000,
-        basal_downstream = 1000, extension = 1000000,
-        extended_tss = NULL, background = NULL, exclude = "gap",
-        cores = 1)
+  # set.seed(123)
+  # gr = randomRegions(nr = 1000, genome = "hg19")
+  # gres <- great(gr, "GO:MF", "hg19")
+    
   
+  # greatResult <- gres
   enrichmentTable <- getEnrichmentTable(greatResult)
-  head(enrichmentTable)
+  # head(enrichmentTable)
+  
+  saveRDS(greatResult, file="greatResult.rds")
+  saveRDS(enrichmentTable, file="enrichmentTable.rds")
   
   cwd <- getwd()
   setwdNew(basename(output$getColumn("Report")))
   on.exit(setwd(cwd), add = TRUE)
-  
-  ### libraries
-  library("rGREAT")
   
   ## Copy the style files and templates
   styleFiles <- file.path(
