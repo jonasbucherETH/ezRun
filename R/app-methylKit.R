7358211d51a66c47defb5924da849a8c4bae9ed4
Jonas Bucher <jobucher@student.ethz.ch>
2023-04-25 14:38:32 +0200

    full test run with saving

--- R/app-methylKit.R
+++ R/app-methylKit.R
@@ -11,6 +11,9 @@ ezMethodMethylKit <- function(input = NA, output = NA, param = NA,
   
   ### libraries
   library("methylKit")
+  library("parallel")
+  library("genomation") # annotation of DML/R
+  
   
   # #setwdNew(basename(output$getColumn("Report")))
   dataset <- input$meta
@@ -35,6 +38,8 @@ ezMethodMethylKit <- function(input = NA, output = NA, param = NA,
                   system.file("extdata", 
                               "control2.myCpG.txt", package = "methylKit"))
   
+  sampleIDs <- list("test1","test2","ctrl1","ctrl2")
+  
   methylRaw <- methRead(file.list,
                  sample.id=list("test1","test2","ctrl1","ctrl2"),
                  assembly="hg18",
@@ -45,13 +50,25 @@ ezMethodMethylKit <- function(input = NA, output = NA, param = NA,
   
   ### read data (final)
   # processBismarkAln / methRead
+  # eg SAM files from Bismark, but also CpG.txt (see above)
   
   
   ##### 2.4 Descriptive statistics on samples
   methylationStats <- getMethylationStats(myobj[[2]],plot=FALSE,both.strands=FALSE)
-  getMethylationStats(myobj[[2]],plot=TRUE,both.strands=FALSE)
-  getCoverageStats(myobj[[2]],plot=TRUE,both.strands=FALSE)
+  # cannot be saved
   
+  # getMethylationStats(myobj[[2]],plot=TRUE,both.strands=FALSE)
+  
+  # coverageStats <- getCoverageStats(myobj[[2]],plot=F,both.strands=FALSE)
+  #### NOTE: this doesn't work, immediately prints and doesn't save
+  # coverageStatsList <- c()
+  # for(i in 1:length(myobj)) { # myobj = list
+  #   name <- paste0("coverageStats_", sampleIDs[i])
+  #   coverageStats <- getCoverageStats(myobj[[i]], plot=F, both.strands=FALSE)
+  #   assign(name, coverageStats)
+  #   coverageStatsList <- cbind(coverageStatsList, name)
+  # }
+
   ##### 2.5 Filtering samples based on read coverage
   filteredMethylRaw  <- filterByCoverage(
       methylRaw,
@@ -61,21 +78,32 @@ ezMethodMethylKit <- function(input = NA, output = NA, param = NA,
       hi.perc=99.9 # Bases/regions having higher coverage than this percentile is discarded
   )
   
+  
+  # normalizeCoverage() function to normalize coverage between samples
+  normalizedMethylRaw <- normalizeCoverage(
+    filteredMethylRaw,
+    method = "median" # median / mean
+    # chunk.size = 1e+06,
+    # save.db = TRUE,
+  )
+  
   ##### 3.1 Merging samples
   # merge all samples to one object for base-pair locations that are covered in all samples
-  methylAll <- unite(methylRaw, destrand=FALSE) # destrand=TRUE (default=FALSE) merges reads on both strands of a CpG dinucleotide
+  methylAll <- unite(normalizedMethylRaw, destrand=FALSE) # destrand=TRUE (default=FALSE) merges reads on both strands of a CpG dinucleotide
   # head(methylAll) # methylBase object
   
   ##### 3.2 Sample Correlation
   sampleCorrelation <- getCorrelation(methylAll, plot=FALSE,
                                       method = "pearson") # default:"pearson", "kendall" and "spearman"
+  ### not saved
   # getCorrelation(methylAll, plot=TRUE, method = "pearson")
   
   ##### 3.3 Clustering samples
   # clusterSamples(methylAll, dist="correlation", method="ward", plot=TRUE)
   clusterDendrogram <- clusterSamples(methylAll, dist="correlation", method="ward", plot=FALSE)
-  PCASamples(methylAll, screeplot=TRUE)
-  PCASamples(methylAll)
+  # PCASamples(methylAll, screeplot=TRUE)
+  # PCASamples(methylAll)
+  
   # If you are convinced that some principal components are accounting for batch effects,
   # you can remove those principal components from your data using removeComp
   
@@ -84,15 +112,29 @@ ezMethodMethylKit <- function(input = NA, output = NA, param = NA,
   sampleAnnotation <- data.frame(batch_id=c("a","a","b","b"),
                               age=c(19,34,23,40))
   
-  as <- assocComp(mBase = methylAll, sampleAnnotation)
-  as
+  batchAssociation <- assocComp(mBase = methylAll, sampleAnnotation)
+
   
   newMethylAll <- removeComp(methylAll, comp=1) # remove PC1
   
   ##### 3.5 Tiling windows analysis -> to get DMRs
   
   ##### 3.6 Finding differentially methylated bases or regions
-  diffMethLoci <- calculateDiffMeth(methylAll)
+  diffMethLoci <- calculateDiffMeth(
+    methylAll,
+    covariates = NULL,
+    overdispersion = "none" # c("none", "MN", "shrinkMN")
+    # adjust = "SLIM", # multiple testing adjustment; default = "SLIM"
+    # c("SLIM", "holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr", "none", "qvalue")
+    # effect = c("wmean", "mean", "predicted"),
+    # test = c("F", "Chisq", "fast.fisher", "midPval"), 
+    # mc.cores = cores,
+    # slim = TRUE, # earlier version
+    # weighted.mean = TRUE, # earlier version
+    # chunk.size = 1e+06, # Number of rows to be taken as a chunk for processing the methylBaseDB objects
+    # save.db = FALSE,
+  )
+  
   # After q-value calculation, we can select the differentially methylated
   # regions/bases based on q-value and percent methylation difference cutoffs
   
@@ -105,6 +147,87 @@ ezMethodMethylKit <- function(input = NA, output = NA, param = NA,
       save.db = FALSE # default=T for methyl(Diff)DB objects; false for methylDiff
       # (...) optional args when save.db = T
   )
+  
+  # also possible per chromosome
+  diffMethPerChr <- diffMethPerChr(diffMethLoci,plot=F,qvalue.cutoff=0.01, meth.cutoff=25)
+  
+  ##### 3.8 Accounting for covariates
+  covariates <- data.frame(age=c(30,80,34,30,80,40))
+  sim.methylBase <- dataSim(replicates=6,sites=1000,
+                          treatment=c(rep(1,3),rep(0,3)),
+                          covariates=covariates,
+                          sample.ids=c(paste0("test",1:3),paste0("ctrl",1:3))
+  )
+  diffMeth_covariates <- calculateDiffMeth(sim.methylBase,
+                                  covariates=covariates,
+                                  overdispersion="MN",test="Chisq",mc.cores=1)
+  
+  ##### 4 Annotating differentially methylated bases or regions
+  gene.obj=readTranscriptFeatures(system.file("extdata", "refseq.hg18.bed.txt", 
+                                              package = "methylKit"))
+  
+  diffMeth_25p_annotated <- annotateWithGeneParts(as(diffMeth_25p,"GRanges"),gene.obj)
+  # Formal class AnnotationByGeneParts
+  
+  # Similarly, we can read the CpG island annotation and
+  # annotate our differentially methylated bases/regions with them.
+  cpg.obj <- readFeatureFlank(system.file("extdata", "cpgi.hg18.bed.txt", 
+                                       package = "methylKit"),
+                           feature.flank.name=c("CpGi","shores"))
+  
+  # convert methylDiff object to GRanges and annotate
+  diffCpGann <- annotateWithFeatureFlank(as(diffMeth_25p,"GRanges"),
+                                      cpg.obj$CpGi,cpg.obj$shores,
+                                      feature.name="CpGi",flank.name="shores")
+  
+  ##### 4.1 Regional analysis
+  promoters <- regionCounts(myobj,gene.obj$promoters)
+  
+  ##### 4.2 Convenience functions for annotation objects
+  diffAnn <- annotateWithGeneParts(as(diffMeth_25p,"GRanges"),gene.obj)
+  # get the distance to TSS and nearest gene name
+  associationWithTSS <- getAssociationWithTSS(diffAnn)
+  
+  # get percentage/number of differentially methylated regions that overlap with intron/exon/promoters
+  targetAnnotationStats <- getTargetAnnotationStats(diffAnn,percentage=TRUE,precedence=TRUE)
+  # promoter       exon     intron intergenic 
+  # 28.15       0.00      11.11      60.74
+  
+  # plot the percentage of differentially methylated bases overlapping with exon/intron/promoters
+  plotTargetAnnotation(diffAnn,precedence=TRUE,
+                       main="differential methylation annotation")
+  
+  # plot the CpG island annotation
+  # shows what percentage of differentially methylated bases
+  # are on CpG islands, CpG island shores and other regions
+  plotTargetAnnotation(diffCpGann,col=c("green","gray","white"),
+                       main="differential methylation annotation")
+  
+  # get percentage of intron/exon/promoters that overlap with differentially methylated bases
+  featsWithTargetsStats <- getFeatsWithTargetsStats(diffAnn,percentage=TRUE)
+  # promoter     exon   intron 
+  # 0.29     0.03     0.17 
+  
+  ########## 5 methylKit convenience functions
+  ##### 5.1 Coercing methylKit objects to GRanges
+  
+  
+  ############## save objects
+  # setwdNew("/home/jobucher/data/MethylKit/doc")
+  # on.exit(setwd(cwd), add = TRUE)
+  
+  saveRDS(filteredMethylRaw, file = paste0("filteredMethylRaw", ".rds"))
+  saveRDS(normalizedMethylRaw, file = paste0("normalizedMethylRaw", ".rds"))
+  saveRDS(methylAll, file = paste0("methylAll", ".rds"))
+  saveRDS(clusterDendrogram, file = paste0("clusterDendrogram", ".rds"))
+  saveRDS(batchAssociation, file = paste0("batchAssociation", ".rds"))
+  saveRDS(diffMethLoci, file = paste0("diffMethLoci", ".rds"))
+  saveRDS(diffMeth_25p, file = paste0("diffMeth_25p", ".rds"))
+  saveRDS(diffMethPerChr, file = paste0("diffMethPerChr", ".rds"))
+  saveRDS(diffMeth_covariates, file = paste0("diffMeth_covariates", ".rds"))
+  saveRDS(gene.obj, file = paste0("gene.obj", ".rds"))
+  saveRDS(diffMeth_25p_annotated, file = paste0("diffMeth_25p_annotated", ".rds"))
+  
   
   
   ## Copy the style files and templates
