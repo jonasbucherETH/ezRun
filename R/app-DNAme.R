70fd6bc5b3af276667bec6f03c1691102cf29de8
Jonas Bucher <jobucher@student.ethz.ch>
2023-07-20 11:37:04 +0200

    setwd(cwd) instead of ..

--- R/app-DNAme.R
+++ R/app-DNAme.R
@@ -144,7 +144,7 @@ ezMethodDNAme <- function(input = NA, output = NA, param = NA,
     } else {
       covColumnName <- paste0("COV_", contexts[i])
     } 
-    setwd("..")
+    setwd(cwd)
     setwd(basename(output$getColumn("Report")))
     setwdNew(contexts[i])
     # ezSystem(paste("mkdir", contexts[i]))
